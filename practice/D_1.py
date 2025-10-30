6d38995d3d14429d05407ff215473c4ee9324cc4
yakhoob32 <yakhoob32@gmail.com>
2025-10-30 15:02:24 +0330

    Day_1

---
+++ practice/D_1.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+
+
+dataset = pd.read_csv('../datasets/Data.csv')
+
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
+
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+X[:, 1:3] = imputer.fit_transform(X[:, 1:3])
+
+LabelEncoder_x = LabelEncoder()
+X[:, 0] = LabelEncoder_x.fit_transform(X[:, 0])
+LabelEncoder_y = LabelEncoder()
+y = LabelEncoder_y.fit_transform(y)
+
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+X = ct.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# print(X)
+
+# print('*'*45)
+# print(X_train)
+
